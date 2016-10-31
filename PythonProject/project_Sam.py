cab76290b5ff21ddd1336eb93da826c75c04f9d4
SamGiezeman <sam.giezeman@gmail.com>
2016-10-31 13:35:11 +0100

    Add files via upload

--- PythonProject/project_Sam.py
+++ PythonProject/project_Sam.py
@@ -5,23 +5,19 @@ import random
 keuze = int
 
 while keuze != 5:
-    print('1: Ik wil een Fiets registreren')
-    print('2: Ik wil mijn fiets stallen')
-    print('3: Ik wil informatie opvragen')
-    print('4: Ik wil mijn fiets ophalen')
-    print('')
+    print('1: Ik wil informatie opvragen')
+    print('2: Ik wil mijn fiets plaatsen')
+    print('3: Ik wil mijn fiets ophalen')
+    print('4: Ik wil mijn fiets registreren')
     try:
         keuze = int(input('voer hier uw keuze in dit moet een getal zijn:'))
     except ValueError:
         print('U heeft geen geldig nummer ingevoerd')
     if keuze == 4:
-        register()#Roept de functie aan die
+        register()#Roept de functie aan die de fiets in het systeem zet
     elif keuze == 1:
-        info()
+        info()#roept de functie die alle informatie laat zien
     elif keuze == 2:
-        stall()
+        stall()#roept de functie aan die je een fiets laat plaatsen
     elif keuze == 3:
-        fetch()
-
-
-# functies die nu erin staan:
+        fetch()#haalt een fiets op uit de stalling

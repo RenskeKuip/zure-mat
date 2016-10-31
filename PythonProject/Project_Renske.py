e0511ffe3733d537a65a46b419a79c36e04d9130
RenskeKuip <renskekuip@gmail.com>
2016-10-31 13:43:45 +0100

    Add files via upload

--- PythonProject/Project_Renske.py
+++ PythonProject/Project_Renske.py
@@ -1,2 +1,9 @@
 def info():
     # vraag de info op
+    for bike in bikeList:
+        print("Je ovNummer is", bikeList[0])
+        if bikeList == True:
+            print("Je fiets is momenteel aanwezig in de stalling.")
+            print("\nDe fiets is aanwezig vanaf", bikeList[3])
+        else:
+            print("Je fiets is momenteel niet aanwezig in de stalling.")

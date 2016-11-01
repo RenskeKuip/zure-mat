5a4e67b1770f9d7b591ce3b79ca7de6f6f89e09c
JasperSmienk <Jasper.Smienk@student.hu.nl>
2016-11-01 16:55:35 +0100

    Add files via upload

--- PythonProject/Project_Jasper.py
+++ PythonProject/Project_Jasper.py
@@ -1,5 +1,6 @@
 import json
 import random
+import datetime
 
 def loadJSON():
     global database
@@ -15,7 +16,7 @@ def writeJSON():
 def randomID():
     global database
     number = ''
-    for loop in range(1):
+    for loop in range(10):
         number += str(random.randrange(0,9))
     if number in database:
         return randomID()
@@ -30,6 +31,19 @@ def register():
     lastName = input('Wat is je achternaam: ')
     name = firstNane + ' ' + lastName
     database[ID] = {'name' : name, 'present' : False}
+    if input('Wil je je fiets gelijk stallen? (Y/N): ') == 'Y':
+        database[ID]['present'] = True
+        database[ID]['since'] = str(datetime.datetime.now())
     writeJSON()
+
+
+def count():
+    global database
+    loadJSON()
+    count = 0
+    for fiets in database:
+        if database[fiets]['present'] is True:
+            count += 1
+    print(count)
 
 register()

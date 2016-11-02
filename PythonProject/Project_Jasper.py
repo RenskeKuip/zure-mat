0186fa4bd2b8f0bed0860ab09c18d1dbcf85a108
JasperSmienk <Jasper.Smienk@student.hu.nl>
2016-11-02 11:28:04 +0100

    Add files via upload

--- PythonProject/Project_Jasper.py
+++ PythonProject/Project_Jasper.py
@@ -9,7 +9,7 @@ def loadJSON():
 
 def writeJSON():
     global database
-    text = json.dumps(database)
+    text = json.dumps(database, sort_keys=True, indent=4)
     with open('database.txt', 'w') as file:
         file.write(text)
 
@@ -27,23 +27,83 @@ def register():
     global database
     loadJSON()
     ID = randomID()
-    firstNane = input('Wat is je voornaam: ')
-    lastName = input('Wat is je achternaam: ')
-    name = firstNane + ' ' + lastName
-    database[ID] = {'name' : name, 'present' : False}
+    name = input('Wat is je voledige naam: ')
+    database[ID] = {'naam' : name, 'aanwezig' : False}
+    print('Het unieke nummer van jouw fiets is: ' + str(ID))
     if input('Wil je je fiets gelijk stallen? (Y/N): ') == 'Y':
-        database[ID]['present'] = True
-        database[ID]['since'] = str(datetime.datetime.now())
+        database[ID]['aanwezig'] = True
+        database[ID]['sinds'] = str(datetime.datetime.now())
     writeJSON()
 
+def store():
+    global database
+    loadJSON()
+    ID = input('Geef het 10 cijferig nummer van jouw de fiets: ')
+    if ID not in database:
+        print('Deze fiets is niet bij ons bekend')
+    elif database[ID]['aanwezig'] is True:
+        print('Deze fiets staat al in de stalling...')
+    else:
+        database[ID]['aanwezig'] = True
+        database[ID]['sinds'] = str(datetime.datetime.now())
+        writeJSON()
+
+def pickup():
+    global database
+    loadJSON()
+    ID = input('Geef het 10 cijferig nummer van de fiets die je wilt ophalen: ')
+    if ID not in database:
+        print('Deze fiets is niet bij ons bekend')
+    elif database[ID]['aanwezig'] is False:
+        print('Deze fiets staat niet in de stalling')
+    else:
+        name = input('Geef jouw voledig name op: ')
+        if database[ID]['naam'] != name:
+            print('Jij bent niet de eigenaar van deze fiets')
+        else:
+            database[ID]['aanwezig'] = False
+            del database[ID]['sinds']
+            print('Je hebt je fiets opgehaald')
+            writeJSON()
 
 def count():
     global database
     loadJSON()
     count = 0
     for fiets in database:
-        if database[fiets]['present'] is True:
+        if database[fiets]['aanwezig'] is True:
             count += 1
-    print(count)
+    if count == 1:
+        print('Er staat op dit moment 1 fiets in de stalling')
+    else:
+       print('Er staan op dit moment ' + str(count) + ' fietsen in de stalling')
+    print('Er zijn nog ' + str(500 - count) + ' plekken beschikbaar')
 
-register()
+
+
+while True:
+    print('----------------------')
+    print('1: Ik wil een fiets registreren')
+    print('2: Ik wil mijn fiets parkeren')
+    print('3: Ik wil mijn fiets ophalen')
+    print('4: Ik wil weten hoeveel plekken er vrij zijn')
+    print('5: Ik wil stoppen')
+    try:
+        menuChoice = int(input(''))
+    except ValueError:
+        print('Geef een geldig nummer op.')
+        continue
+    if menuChoice < 1 or menuChoice > 5:
+        print('Geef een geldig nummer op.')
+        continue
+    elif menuChoice == 1:
+        register()
+    elif menuChoice == 2:
+        store()
+    elif menuChoice == 3:
+        pickup()
+    elif menuChoice == 4:
+        count()
+    elif menuChoice == 5:
+        print('Bedankt en tot ziens!')
+        break

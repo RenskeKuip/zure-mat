b16657917eaf459257c848dfb5086942e447f238
RenskeKuip <renskekuip@gmail.com>
2016-11-04 02:21:10 +0100

    Register almost works!

--- PythonProject/test for entry 2.py
+++ PythonProject/test for entry 2.py
@@ -8,6 +8,8 @@ import sys
 chipkaartnummer = ""
 name = ""
 counter = 0
+counter1 = 0
+ID = ""
 
 def loadJSON():
     global database
@@ -21,7 +23,6 @@ def writeJSON():
         file.write(text)
 
 def randomID():
-    window = Toplevel(root)
     global database
     number = ''
     for loop in range(10):
@@ -55,18 +56,15 @@ def register():
             print(entry1.get())
             global name
             name = entry1.get()
+            fietsCode()
 
         b1 = Button(window, text="continue", command= lambda: callback())
         b1.pack()
-
-    if counter > 0:
-        nameAsk()
-    global counter
-    counter += 1
-    chipscan()
-    ID = randomID()
+        # ID = randomID()
+        # counter += 1
 
     def fietsCode():
+        ovChip = chipkaartnummer
         database[ID] = {'naam' : name, 'aanwezig' : False, 'ovchip' : ovChip}
         showinfo(message='Het unieke nummer van jouw fiets is: ' + str(ID))
         if input('Wil je je fiets gelijk stallen? (Y/N): ') == 'Y':
@@ -74,8 +72,15 @@ def register():
             database[ID]['sinds'] = str(datetime.datetime.now())
         writeJSON()
 
+    if counter > 0:
+        nameAsk()
 
-ID = ""
+    global counter
+    counter += 1
+    if counter1 <= 0:
+        chipscan()
+    ID = randomID()
+
 def store():
     global database
     loadJSON()
@@ -136,59 +141,81 @@ def count():
 
 
 def chipscan():
+    root.withdraw()
+    #haalt root weg, terug krijgen is root.deiconify()
+    global counter1
+    counter1 += 1
+    # counter1 is zodat ie niet nog een keer door bepaalde code loopt
     window = Toplevel(root)
     Label(window, text="Dit leest de ov-chipkaart scanner? (voer hier een het getal van je ov chipkaart in zonder de spaties): ").pack(ipady=5, ipadx=5, padx=2, pady=2)
     entry = Entry(window)
     entry.pack()
 
-    def callback1():
-        print(entry.get())
-
+    def callback1():    # geeft chipkaartnummer waarde van entry
+        # print(entry.get())
         global chipkaartnummer
         chipkaartnummer = entry.get()
 
         if len(chipkaartnummer) == 16:
             try:
-                chipkaartnummer = int(chipkaartnummer)
-                return chipkaartnummer
+                int(chipkaartnummer)
+                # print(chipkaartnummer, "you did it!")
                 register()
+                # return chipkaartnummer
             except:
                 showinfo(message='U heeft geen getal ingevoerd')
+                print("GEEN GETAL")  # is voor test
+                quit(window)
                 chipscan()
         else:
             showinfo(message='De code was niet 16 tekens lang ')
+            print("NIET LANG")  # is test
+            # quit(window)
             chipscan()
 
     b1 = Button(window, text="submit", command= lambda: callback1())
     b1.pack()
-
+    # root.deiconify()
 
 root = Tk()
+nsBlue = '#002D72'
+nsYellow = '#FFC72C'
+root.configure(bg=nsYellow)
 
 label = Label(master=root,
               text='Fietsenstalling',
               font =("Italic",50),
+              foreground=nsBlue,
+              background=nsYellow,
               width=15,
               height=5)
 label.pack()
 
 button = Button(master=root,
                 text="Fiets registreren",
+                foreground=nsBlue,
+                background=nsYellow,
                 command=register)
 button.pack(ipady=5, ipadx=5, padx=2, pady=2)
 
 button2 = Button(master=root,
                  text="Fiets stallen",
-                command=store)
+                 foreground=nsBlue,
+                 background=nsYellow,
+                 command=store)
 button2.pack(ipady=5, ipadx=5, padx=2, pady=2)
 
 button3 = Button(master=root,
                  text="Fiets ophalen",
-                command=pickup)
+                 foreground=nsBlue,
+                 background=nsYellow,
+                 command=pickup)
 button3.pack(ipady=5, ipadx=5, padx=2, pady=2)
 
 button4 =  Button(master=root,
                   text="Informatie opvragen",
+                  foreground=nsBlue,
+                  background=nsYellow,
                   command=count)
 button4.pack(ipady=5, ipadx=5, padx=2, pady=2)
 
